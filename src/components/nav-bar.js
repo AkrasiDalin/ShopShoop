0df2b429cffa49ca5cc5425a72899f4f626a6bf6
AkDalin <dalin.jo@yahoo.com>
2022-01-09 09:32:34 +0000

    Beautify search bar

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -10,13 +10,15 @@ const NavBar = () => {
     return (
         <div className=" w-100">
             <NavBarCSS className="justify-content-center">
-                <form  className="p-2 col-4 d-block">
-                    
+            <div className="back"></div>
+            <form  className="p-2 col-4 d-block">
+                <div className="p-2 bg-light rounded rounded-pill">
                     <div className="input-group">
                         <input type="search" placeholder="What are you looking for?" className="form-control border-0 bg-light"/>
                         <button type="submit" className="btn btn-link text-primary"><i className="bi bi-search"></i></button>
                     </div>
-                </form>
+                </div>
+            </form>
             </NavBarCSS>
         </div>
         
