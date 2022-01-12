7408f3df0b1910ee23547839444c6408187fd526
AkDalin <dalin.jo@yahoo.com>
2022-01-12 09:38:32 +0000

    Correct header styling

--- src/components/header.js
+++ src/components/header.js
@@ -4,16 +4,16 @@ import NavBar from "./nav-bar";
 const Header = () => {
     
     return (
-        <NavBarCSS className="fix-top  row row-col-8 justify-content-center">
-            <NavBar />
-        </NavBarCSS>
+            <NavBarCSS className="fix-top  row row-col-8 justify-content-center">
+                <NavBar />
+            </NavBarCSS>
     )
 }
 
 const NavBarCSS = styled.div`
-    background-color: yellow;
     width: 100%;
-    height: 100px;
+    height: 80px;
+    margin-bottom: 20px;
 `;
 
 export default Header;
