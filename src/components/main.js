ba2b561c040f12f7de0388432dd6e98067c9d35b
AkDalin <dalin.jo@yahoo.com>
2022-01-12 09:42:00 +0000

    Use fakeapi restapi to fetch sample products and replace placeholders

--- src/components/main.js
+++ src/components/main.js
@@ -1,25 +1,25 @@
 import styled from "styled-components";
 
-const Main = () => {
-    
+const Main = (props) => {
     return (
         <section className="container col-7" >
             <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            {[34,54,34,45,32,54,34,5,3,56,34,65,43,65,34,6,3,34,54,34,45,32,54,34,5,3,56,34,65,43,65,34,6,3,34,54,34,45,32,54,34,5,3,56,34,65,43,65,34,6,3,34,54,34,45,32,54,34,5,3,56,34,65,43,65,34,6,3,34,54,34,45,32,54,34,5,3,56,34,65,43,65,34,6,3,34,54,34,45,32,54,34,5,3,56,34,65,43,65,34,6,3,34,54,34,45,32,54,34,5,3,56,34,65,43,65,34,6,3,34,54,34,45,32,54,34,5,3,56,34,65,43,65,34,6,3,34,54,34,45,32,54,34,5,3,56,34,65,43,65,34,6,3,34,54,34,45,32,54,34,5,3,56,34,65,43,65,34,6,3].map((x,y) => (
-                <div className="col mb-5" key={y}>
+            {props.data.map((obj,y) => (
+                <MainCSS className="col mb-5" key={obj.id}>
                     <div className="card h-100">
-                        <img className="card-img-top" src="" alt="..." />
-                        <div className="card-body p-4">
+                        <img className="card-img-top" src={obj.image} alt={obj.title} />
+                        <div className="card-body p-2">
                             <div className="text-center">
-                                <h5 className="fw-bolder">Product Name</h5>
-                                $price
+                                <h5 className="fw-bolder">{obj.title}</h5>
+                                £{obj.price}
+                            </div>
+                            <div className="card-footer p-2 pt-2 border-top-0 bg-transparent">
+                                <div className="text-center"><a className="btn mt-auto "  href="#">Add to cart</a></div>
                             </div>
                         </div>
-                        <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                        </div>
+                        
                     </div>
-                </div>
+                </MainCSS>
             ))}
             </div>
         </section>
@@ -27,10 +27,40 @@ const Main = () => {
 }
 
 const MainCSS = styled.div`
-    background-color: violet;
-    flex: 1 32%;
-    max-width: 1000px;
-    min-height: 700px;
+    img {
+        width: 200px;
+        height: 200px;
+        margin: 0 auto;
+        padding: 10px;
+    }
+
+    h5 {
+        // width: 180px;
+        white-space: nowrap;
+        overflow: hidden !important;
+        text-overflow: ellipsis;
+    }
+
+    .card {
+        border: 0;
+        border-radius: 25px;
+        
+        .card-body {
+            background-image: linear-gradient(to right, #2fcbc4, #3fa9a5);
+            color: white;
+        }
+        .card-footer  {
+            background-color: #f58d46;
+            height: 50px;
+        }
+    
+        .card-footer a {
+            background-color:#edc522;
+            // color: white;
+        }
+        
+    }
+    
 `;
 
 export default Main;
